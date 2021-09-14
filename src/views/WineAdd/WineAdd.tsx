eb4e39efa079437a0a0c331f64cacbfbf4f143ea
vithujey <vithushjeytharma@gmail.com>
2021-09-14 13:25:27 +0530

    fix: main layout

--- src/views/WineAdd/WineAdd.tsx
+++ src/views/WineAdd/WineAdd.tsx
@@ -1,15 +1,16 @@
 import { Row } from "antd";
 import React from "react";
+import FormLayout from "../../components/ContentLayouts/FormLayout";
 import "./WineAdd.style.less";
 
 function WineAdd() {
   return (
-    <div>
-      <Row className="content-header" justify="space-between">
-        <p className="content-title">Add Wine</p>
-      </Row>
-      <Row className="content-body"></Row>
-    </div>
+    <FormLayout
+      title="Add Wine"
+      coverImgSrc="https://images.unsplash.com/photo-1598152541316-921ebb5e0433?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+    >
+      <></>
+    </FormLayout>
   );
 }
 
